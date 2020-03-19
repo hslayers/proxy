03d4fe9fe9c93deb795da8d8172c4101359d7f99
raitis <raitisbe@gmail.com>
2020-03-19 16:42:12 +0200

    If slash is missing in http:/domain then add it

--- main.js
+++ main.js
@@ -1,14 +1,35 @@
 
 // Listen on a specific host via the HOST environment variable
-const host = process.env.HOST || '0.0.0.0';
+const host = '0.0.0.0';
 // Listen on a specific port via the PORT environment variable
 const port = process.env.PROXY_PORT || 8085;
+const querystring = require('querystring');
 
-const cors_proxy = require('cors-anywhere');
-cors_proxy.createServer({
-    originWhitelist: [], // Allow all origins
-    //requireHeader: ['origin', 'x-requested-with'],
-    removeHeaders: ['cookie', 'cookie2']
-}).listen(port, host, function() {
-    console.log('Running CORS Anywhere on ' + host + ':' + port);
+const cors_proxy = require('cors-anywhere').createServer({
+  originWhitelist: [], // Allow all origins
+  //requireHeader: ['origin', 'x-requested-with'],
+  //removeHeaders: ['cookie', 'cookie2']
+});
+const geonamesUsername = 'raitis';
+
+require('http').createServer(function (req, res) {
+  try {
+    req.url = req.url.replace('/mapproxy', '');
+    if (req.url.indexOf('/search') > -1) {
+      const params = querystring.decode(req.url.split('?')[1]);
+      if (typeof params.provider == 'undefined' || params.provider == 'geonames')
+        req.url = `/http://api.geonames.org/searchJSON?name_startsWith=${encodeURIComponent(params.q)}&username=${geonamesUsername}`
+    }
+    if (req.url.indexOf(':/') > -1 && req.url.indexOf('://') == -1) {
+      req.url = req.url.replace(':/', '://');
+    }
+    console.log(req.url);
+    cors_proxy.emit('request', req, res);
+  } catch (ex) {
+    res.writeHead(500, { 'Content-Type': 'text/plain' });
+    res.write('Invalid request');
+    res.end();
+  }
+}).listen(port, host, function () {
+  console.log('Running CORS Anywhere on ' + host + ':' + port);
 });
